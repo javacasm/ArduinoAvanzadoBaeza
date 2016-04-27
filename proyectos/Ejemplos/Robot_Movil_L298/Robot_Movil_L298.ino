8d12cd148aa12d7ec4a0c6cbeba20b64a1279d0a
javacasm <javacasm@gmail.com>
2016-04-27 22:37:10 +0200

    US

--- proyectos/Ejemplos/Robot_Movil_L298/Robot_Movil_L298.ino
+++ proyectos/Ejemplos/Robot_Movil_L298/Robot_Movil_L298.ino
@@ -7,6 +7,8 @@ int motor1_velocidad=11;//  EnA
 int motor2_velocidad=12;//  EnB
 
 int sensorIRSuelo=6;
+int sensorUSTrigger=5;
+int sensorUSEcho=4;
 
 void adelante()
 {
@@ -31,6 +33,29 @@ void atras()
   digitalWrite(motor2_atras,HIGH);
 }
 
+void derecha()
+{
+  // motor 1 en dirección atras
+  digitalWrite(motor1_adelante,LOW);
+  digitalWrite(motor1_atras,HIGH);
+
+  // motor 2 en dirección adelante
+  digitalWrite(motor2_adelante,HIGH);
+  digitalWrite(motor2_atras,LOW);
+}
+
+
+void parar()
+{
+  // motor 1 parado
+  digitalWrite(motor1_adelante,LOW);
+  digitalWrite(motor1_atras,LOW);
+
+  // motor 2 parado
+  digitalWrite(motor2_adelante,LOW);
+  digitalWrite(motor2_atras,LOW);
+}
+
 int dv=0; // Los motores se mueven a la misma velocidad
 void velocidad(int v)
 {
@@ -41,25 +66,57 @@ void velocidad(int v)
 }
 
 void setup() {
-  // Fundamental!!! ACTIVAR LAS PATILLAS COMO SALIDAS
+  // Fundamental!!! ACTIVAR LAS PATILLAS DE CONTROL DE LOS MOTORES COMO SALIDAS
   pinMode(motor1_adelante,OUTPUT);
   pinMode(motor1_atras,OUTPUT);
   pinMode(motor2_adelante,OUTPUT);
   pinMode(motor2_atras,OUTPUT);
+
+  // Sensores
+  pinMode(sensorIRSuelo,INPUT);
+  pinMode(sensorUSTrigger,INPUT);
+  pinMode(sensorUSEcho,INPUT);
  
 }
 
 int haySuelo()
 {
   int resultado=0;
-  if(digitalRead(SensorSuelo)==HIGH) // Detectamos el suelo
+  if(digitalRead(sensorIRSuelo)==HIGH) // Detectamos el suelo
   {  resultado=1; }
   else
   {  resultado=0;}
   
   return resultado;
 }
+
+int mideDistancia()
+{
+  // Mejor usar la librería newPing
+  
+}
+
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if(haySuelo())
+  {
+      if(mideDistancia()<10)
+      {
+        adelante();
+      }
+      else
+      {
+        derecha();
+        delay(300);
+      }
+    
+  }
+  else  // Si no hay suelo paramos y volvemos hacia atrás
+  {
+      parar();
+      atras();
+      delay(1000);
+      derecha();
+      delay(300);
+  }
 }
